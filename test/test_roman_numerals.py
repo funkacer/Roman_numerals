4d79cda783dea0e4ac52240a85e9df4e43579e82
Jiri Cerny <funkacer@gmail.com>
2021-05-30 10:07:52 +0200

    Add correct reference to test cases for pytest

--- test/test_roman_numerals.py
+++ test/test_roman_numerals.py
@@ -1,9 +1,16 @@
 import unittest
+import os
+import sys
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from src.roman_numerals import roman_to_arabic, arabic_to_roman
 
 class KnownValues(unittest.TestCase):
-    with open ('Test_cases.tst', 'r') as f:
+        
+    with open (os.path.join(SCRIPT_DIR,'test_cases.tst'), 'r') as f:
         lines = f.read().split()
     known_values = []
     for line in lines[1:]:  # first line are column names
